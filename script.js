3a7f6f1471c48ea2f4cd5483bb3f5ef9fd1b9f08
MartinAcostaDEV <martinacostadev@gmail.com>
2023-01-25 21:41:57 -0300

    Modificado script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+function constructorObjeto(nombre, edad){
+    this.nombre = nombre;
+    this.edad = edad;
+}
+
+
 boton = document.querySelector("#botoncito");
 
 boton.addEventListener('click', function(){
-    fondo = document.querySelector("body");
-    if (fondo.style.backgroundColor == "red"){
-        fondo.style.backgroundColor = "blue"
-    }else{
-        fondo.style.backgroundColor = "red"
-    }
+    let nombre = prompt("Introduzca el nombre del sujeto");
+    let edad = prompt("Introduzca la edad del sujeto");
+
+    const objeto1 = new constructorObjeto(nombre, edad);
+
+    console.log("El nombre es: "+objeto1.nombre+". Y su edad es: "+objeto1.edad);
 }); 
 
