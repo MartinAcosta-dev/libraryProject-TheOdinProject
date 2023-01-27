f4e4422851a844b6832a0367cff8624ca730cf42
MartinAcostaDev <martinacostadev@gmail.com>
2023-01-27 15:50:16 -0300

    Comiteado

--- script.js
+++ script.js
@@ -1,40 +1,10 @@
-function constructorBook(titulo, autor, paginas, leido){
-    this.titulo = titulo;
-    this.autor = autor;
-    this.paginas = paginas;
-    this.leido = leido;
-}
+$(document).ready(function(){
 
-constructorBook.prototype.getInfo = function(){
-    let respuesta = "";
 
-    respuesta += "El titulo del libro es: "+this.titulo+". <br> Su autor es: "+this.autor+". <br> Su numero de paginas es: "+this.paginas;
+    $("#btnAddBook").click(function(){
+        let variable = $(".libros").find("#caja2");
+        console.log(variable);
+    });
 
-    if(this.leido == false){
-        respuesta += "<br> Aun no ha leido este libro.";
-    }else{
-        respuesta += "<br> Ya ha leido este libro!!";
-    }
-    return respuesta;
-}
 
-boton = document.querySelector("#botoncito");
-
-boton.addEventListener('click', function(){
-    let titulo = prompt("Introduzca el nombre del libro");
-    let autor = prompt("Introduzca el autor del libro");
-    let paginas = prompt("Introduzca el numero de paginas");
-    let leido = confirm("Ha leido este libro?")
-
-    const objeto1 = new constructorBook(titulo, autor, paginas, leido);
-
-    console.log(objeto1.getInfo());
-
-    let plantilla = `<h1>
-                        ${objeto1.getInfo()}
-                     <h1>`;
-
-    let pagina = document.querySelector("body");
-    pagina.innerHTML = plantilla;
-}); 
-
+});
